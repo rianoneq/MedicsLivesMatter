410398acd79f5004f285747b9ea5d674e3bdb45e
itRianone <timokhindimitry@gmail.com>
2021-03-25 11:28:41 +0300

    anchors

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+
+  $('a[href^="#"]').click(function () {
+    let anchor = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(anchor).offset().top
+    }, 600);
+  });
+
   $('.slider').slick({
     dots: true,
     slidesToShow: 1,
